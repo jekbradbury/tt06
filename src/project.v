dbf0a3921000ac1b7951e8118130a28073863d57
James Bradbury <jek@anthropic.com>
2024-04-14 15:54:33 -0700

    update to tt06 interface

--- src/project.v
+++ src/project.v
@@ -19,52 +19,14 @@ module tt_um_cchan_fp8_multiplier (
     assign uio_out = 0;
     assign uio_oe  = 0;
 
-    // TODO: Now that we can have two 8-bit inputs and a separate clock, we can simplify this module a lot.
-
-    wire clk2 = ui_in[0];
-    wire [2:0] ctrl = ui_in[3:1];
-    wire [3:0] data = ui_in[7:4];
-    // wire [6:0] led_out;
-    // assign io_out[6:0] = led_out;
-    // wire [5:0] seed_input = io_in[7:2];
-
-    reg [8:0] operand1;
-    reg [8:0] operand2;
-    // For now we're commenting this out and leaving the results unbuffered.
-    // reg [8:0] result_out;
-    // assign io_out = result_out;
-
-    always @(posedge clk2) begin
-        if (!ctrl[0]) begin  // if first CTRL bit is off, we're in STORE mode
-            if (!ctrl[1]) begin  // second CTRL bit controls whether it's the first or second operand
-                if (!ctrl[2]) begin  // third CTRL bit controls whether it's the upper or lower half
-                    operand1[3:0] <= data;
-                end else begin
-                    operand1[7:4] <= data;
-                end
-            end else begin
-                if (!ctrl[2]) begin
-                    operand2[3:0] <= data;
-                end else begin
-                    operand2[7:4] <= data;
-                end
-            end
-        end else begin  // if first CTRL bit is on, this is reserved.
-            // TODO
-            // if (!ctrl[1] && !ctrl[2]) begin
-            //     result_out[7:0] <= 0;
-            // end
-        end
-    end
-
-    // Compute result_out in terms of operand1, operand2
+    // Compute result_out in terms of ui_in, uio_in
     fp8mul mul1(
-        .sign1(operand1[7]),
-        .exp1(operand1[6:3]),
-        .mant1(operand1[2:0]),
-        .sign2(operand2[7]),
-        .exp2(operand2[6:3]),
-        .mant2(operand2[2:0]),
+        .sign1(ui_in[7]),
+        .exp1(ui_in[6:3]),
+        .mant1(ui_in[2:0]),
+        .sign2(uio_in[7]),
+        .exp2(uio_in[6:3]),
+        .mant2(uio_in[2:0]),
         .sign_out(uo_out[7]),
         .exp_out(uo_out[6:3]),
         .mant_out(uo_out[2:0])
