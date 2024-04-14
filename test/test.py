53ade83f20ca39071032ee1245dccb765c85d46d
James Bradbury <jek@anthropic.com>
2024-04-14 16:02:30 -0700

    fix the test

--- test/test.py
+++ test/test.py
@@ -3,6 +3,7 @@
 
 import cocotb
 from cocotb.triggers import Timer
+from cocotb.binary import BinaryValue
 from model import get_8bit_op, to_binary, to_float
 
 
@@ -10,27 +11,13 @@ from model import get_8bit_op, to_binary, to_float
 async def test_all_inputs(dut):
     """Test all possible pairs of 8-bit inputs."""
 
-    async def store(operand, half, data):
-        dut.ui_in[0].value = 0
-        dut.ui_in[1].value = 0
-        dut.ui_in[2].value = operand
-        dut.ui_in[3].value = half
-        for i in range(4):
-            dut.ui_in[4+i].value = data[i]
-        await Timer(1, units="ms")
-        dut.ui_in[0].value = 1
-        await Timer(1, units="ms")
-
     fp8_mul_model = get_8bit_op(lambda a, b: a * b)
 
     # TODO: Test in random order
     for i in range(256):
         for j in range(256):
-            in1 = to_binary(i)
-            in2 = to_binary(j)
-            await store(0, 0, in1[:4])
-            await store(0, 1, in1[4:])
-            await store(1, 0, in2[:4])
-            await store(1, 1, in2[4:])
+            dut.ui_in.value = BinaryValue(to_binary(i))
+            dut.uio_in.value = BinaryValue(to_binary(j))
+            await Timer(1, units="ms")
             correct = fp8_mul_model(i, j)
             assert dut.uo_out.value.binstr == f"{correct:08b}", f"{to_float(i)} ({i:08b}) * {to_float(j)} ({j:08b}) = {to_float(dut.uo_out.value.integer)} ({dut.uo_out.value.integer:08b}), should be {to_float(correct)} ({correct:08b})"
